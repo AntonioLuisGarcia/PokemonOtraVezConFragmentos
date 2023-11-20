680e2bcfbdd8e6d3a9ced4cc8a1e922ecee3e35c
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-11-20 10:56:11 +0100

    PokemonDetail with bigImg

---
+++ app/src/main/java/com/turing/alan/pokemonotravezconfragmentos/data/api/PokemonDetail.kt
@@ -0,0 +1,9 @@
+package com.turing.alan.pokemonotravezconfragmentos.data.api
+
+data class PokemonDetail(
+    val id:Int,
+    val name:String,
+    val weight:Int,
+    val height:Int,
+    val img: String,
+)
